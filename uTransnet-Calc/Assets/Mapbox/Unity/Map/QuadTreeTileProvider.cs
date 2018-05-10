4424e59aa78d7d4da9632d34e32afd80a621d084
Artem <artego@fuit.spbstu.ru>
2018-05-10 13:40:25 +0300

    UTG-84: add Modifier for vector layer to not to intersect with terrain, add pause between zoom change and tiles loading.

--- uTransnet-Calc/Assets/Mapbox/Unity/Map/QuadTreeTileProvider.cs
+++ uTransnet-Calc/Assets/Mapbox/Unity/Map/QuadTreeTileProvider.cs
@@ -1,144 +1,173 @@
-﻿namespace Mapbox.Unity.Map
+﻿using System;
+
+namespace Mapbox.Unity.Map
 {
-	using UnityEngine;
-	using Mapbox.Map;
-	using Mapbox.Unity.Utilities;
-	using Mapbox.Utils;
-	using System.Collections.Generic;
-	using System.Linq;
-	using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Mapbox.Map;
+    using Mapbox.Unity.Utilities;
+    using Mapbox.Utils;
+    using UnityEngine;
 
-	public class QuadTreeTileProvider : AbstractTileProvider
-	{
-		Plane _groundPlane;
-		float _elapsedTime;
-		bool _shouldUpdate;
+    public class QuadTreeTileProvider : AbstractTileProvider
+    {
+        Plane _groundPlane;
+        float _elapsedTime;
+        bool _shouldUpdate;
 
-		CameraBoundsTileProviderOptions _cbtpOptions;
+        float _oldZoom = 0;
 
-		public override void OnInitialized()
-		{
-			_cbtpOptions = (CameraBoundsTileProviderOptions)_options;
-			if (_cbtpOptions.camera == null)
-			{
-				_cbtpOptions.camera = Camera.main;
-			}
-			_groundPlane = new Plane(Vector3.up, 0);
-			_shouldUpdate = true;
-		}
+        CameraBoundsTileProviderOptions _cbtpOptions;
 
-		void Update()
-		{
-			//Camera Debugging
-			//Vector3[] frustumCorners = new Vector3[4];
-			//_cbtpOptions.camera.CalculateFrustumCorners(new Rect(0, 0, 1, 1), _cbtpOptions.camera.transform.position.y, Camera.MonoOrStereoscopicEye.Mono, frustumCorners);
+        public override void OnInitialized()
+        {
+            _cbtpOptions = (CameraBoundsTileProviderOptions)_options;
+            if (_cbtpOptions.camera == null)
+            {
+                _cbtpOptions.camera = Camera.main;
+            }
+            _groundPlane = new Plane(Vector3.up, 0);
+            _shouldUpdate = true;
+            _oldZoom = _map.Zoom;
+        }
 
-			//for (int i = 0; i < 4; i++)
-			//{
-			//	var worldSpaceCorner = _cbtpOptions.camera.transform.TransformVector(frustumCorners[i]);
-			//	Debug.DrawRay(_cbtpOptions.camera.transform.position, worldSpaceCorner, Color.blue);
-			//}
+        void Update()
+        {
+            //Camera Debugging
+            //Vector3[] frustumCorners = new Vector3[4];
+            //_cbtpOptions.camera.CalculateFrustumCorners(new Rect(0, 0, 1, 1), _cbtpOptions.camera.transform.position.y, Camera.MonoOrStereoscopicEye.Mono, frustumCorners);
 
-			if (!_shouldUpdate)
-			{
-				return;
-			}
+            //for (int i = 0; i < 4; i++)
+            //{
+            //	var worldSpaceCorner = _cbtpOptions.camera.transform.TransformVector(frustumCorners[i]);
+            //	Debug.DrawRay(_cbtpOptions.camera.transform.position, worldSpaceCorner, Color.blue);
+            //}
 
-			_elapsedTime += Time.deltaTime;
+            if (!_shouldUpdate)
+            {
+                return;
+            }
 
-			if (_elapsedTime >= _cbtpOptions.updateInterval)
-			{
-				_elapsedTime = 0f;
+            _elapsedTime += Time.deltaTime;
 
-				//update viewport in case it was changed by switching zoom level
-				Vector2dBounds _viewPortWebMercBounds = getcurrentViewPortWebMerc();
+            if (ShouldUpdate())
+            {
 
-				var tilesToRequest = TileCover.GetWithWebMerc(_viewPortWebMercBounds, _map.AbsoluteZoom);
+                _oldZoom = _map.Zoom;
+                _elapsedTime = 0f;
 
-				var activeTiles = _activeTiles.Keys.ToList();
-				List<UnwrappedTileId> toRemove = activeTiles.Except(tilesToRequest).ToList();
-				foreach (var t2r in toRemove) { RemoveTile(t2r); }
-				var finalTilesNeeded = tilesToRequest.Except(activeTiles);
+                //update viewport in case it was changed by switching zoom level
+                Vector2dBounds _viewPortWebMercBounds = getcurrentViewPortWebMerc();
 
-				foreach (var tile in activeTiles)
-				{
-					// Reposition tiles in case we panned.
-					RepositionTile(tile);
-				}
+                var tilesToRequest = TileCover.GetWithWebMerc(_viewPortWebMercBounds, _map.AbsoluteZoom);
 
-				foreach (var tile in finalTilesNeeded)
-				{
-					AddTile(tile);
-				}
-			}
-		}
+                var activeTiles = _activeTiles.Keys.ToList();
+                List<UnwrappedTileId> toRemove = activeTiles.Except(tilesToRequest).ToList();
+                foreach (var t2r in toRemove)
+                {
+                    RemoveTile(t2r);
+                }
+                var finalTilesNeeded = tilesToRequest.Except(activeTiles);
 
-		private Vector2dBounds getcurrentViewPortWebMerc(bool useGroundPlane = true)
-		{
-			Vector3[] hitPnt = new Vector3[4];
+                foreach (var tile in activeTiles)
+                {
+                    // Reposition tiles in case we panned.
+                    RepositionTile(tile);
+                }
 
-			if (useGroundPlane)
-			{
-				// rays from camera to groundplane: lower left and upper right
-				Ray ray00 = _cbtpOptions.camera.ViewportPointToRay(new Vector3(0, 0));
-				Ray ray01 = _cbtpOptions.camera.ViewportPointToRay(new Vector3(0, 1));
-				Ray ray10 = _cbtpOptions.camera.ViewportPointToRay(new Vector3(1, 0));
-				Ray ray11 = _cbtpOptions.camera.ViewportPointToRay(new Vector3(1, 1));
-				hitPnt[0] = getGroundPlaneHitPoint(ray00);
-				hitPnt[1] = getGroundPlaneHitPoint(ray01);
-				hitPnt[2] = getGroundPlaneHitPoint(ray10);
-				hitPnt[3] = getGroundPlaneHitPoint(ray11);
-			}
+                foreach (var tile in finalTilesNeeded)
+                {
+                    AddTile(tile);
+                }
+            }
+        }
 
-			// Find min max bounding box. 
-			// TODO : Find a better way of doing this. 
-			float minX = float.MaxValue;
-			float minZ = float.MaxValue;
-			float maxX = float.MinValue;
-			float maxZ = float.MinValue;
-			for (int i = 0; i < 4; i++)
-			{
-				if (minX > hitPnt[i].x)
-				{
-					minX = hitPnt[i].x;
-				}
+        private Vector2dBounds getcurrentViewPortWebMerc(bool useGroundPlane = true)
+        {
+            Vector3[] hitPnt = new Vector3[4];
 
-				if (minZ > hitPnt[i].z)
-				{
-					minZ = hitPnt[i].z;
-				}
+            if (useGroundPlane)
+            {
+                // rays from camera to groundplane: lower left and upper right
+                Ray ray00 = _cbtpOptions.camera.ViewportPointToRay(new Vector3(0, 0));
+                Ray ray01 = _cbtpOptions.camera.ViewportPointToRay(new Vector3(0, 1));
+                Ray ray10 = _cbtpOptions.camera.ViewportPointToRay(new Vector3(1, 0));
+                Ray ray11 = _cbtpOptions.camera.ViewportPointToRay(new Vector3(1, 1));
+                hitPnt[0] = getGroundPlaneHitPoint(ray00);
+                hitPnt[1] = getGroundPlaneHitPoint(ray01);
+                hitPnt[2] = getGroundPlaneHitPoint(ray10);
+                hitPnt[3] = getGroundPlaneHitPoint(ray11);
+            }
 
-				if (maxX < hitPnt[i].x)
-				{
-					maxX = hitPnt[i].x;
-				}
+            // Find min max bounding box.
+            // TODO : Find a better way of doing this.
+            float minX = float.MaxValue;
+            float minZ = float.MaxValue;
+            float maxX = float.MinValue;
+            float maxZ = float.MinValue;
+            for (int i = 0; i < 4; i++)
+            {
+                if (minX > hitPnt[i].x)
+                {
+                    minX = hitPnt[i].x;
+                }
 
-				if (maxZ < hitPnt[i].z)
-				{
-					maxZ = hitPnt[i].z;
-				}
-			}
+                if (minZ > hitPnt[i].z)
+                {
+                    minZ = hitPnt[i].z;
+                }
 
-			Vector3 hitPntLL = new Vector3(minX, 0, minZ);
-			Vector3 hitPntUR = new Vector3(maxX, 0, maxZ);
+                if (maxX < hitPnt[i].x)
+                {
+                    maxX = hitPnt[i].x;
+                }
 
-			//Debug.Log(hitPntLL + " - " + hitPntUR);
+                if (maxZ < hitPnt[i].z)
+                {
+                    maxZ = hitPnt[i].z;
+                }
+            }
 
-			var llLatLong = _map.WorldToGeoPosition(hitPntLL);
-			var urLatLong = _map.WorldToGeoPosition(hitPntUR);
+            Vector3 hitPntLL = new Vector3(minX, 0, minZ);
+            Vector3 hitPntUR = new Vector3(maxX, 0, maxZ);
 
-			Vector2dBounds tileBounds = new Vector2dBounds(Conversions.LatLonToMeters(llLatLong), Conversions.LatLonToMeters(urLatLong));
+            //Debug.Log(hitPntLL + " - " + hitPntUR);
 
-			// Bounds debugging. 
-			Debug.DrawLine(_cbtpOptions.camera.transform.position, hitPntLL, Color.blue);
-			Debug.DrawLine(_cbtpOptions.camera.transform.position, hitPntUR, Color.red);
-			return tileBounds;
-		}
-		private Vector3 getGroundPlaneHitPoint(Ray ray)
-		{
-			float distance;
-			if (!_groundPlane.Raycast(ray, out distance)) { return Vector3.zero; }
-			return ray.GetPoint(distance);
-		}
-	}
+            var llLatLong = _map.WorldToGeoPosition(hitPntLL);
+            var urLatLong = _map.WorldToGeoPosition(hitPntUR);
+
+            Vector2dBounds tileBounds = new Vector2dBounds(Conversions.LatLonToMeters(llLatLong), Conversions.LatLonToMeters(urLatLong));
+
+            // Bounds debugging.
+            Debug.DrawLine(_cbtpOptions.camera.transform.position, hitPntLL, Color.blue);
+            Debug.DrawLine(_cbtpOptions.camera.transform.position, hitPntUR, Color.red);
+            return tileBounds;
+        }
+
+        private Vector3 getGroundPlaneHitPoint(Ray ray)
+        {
+            float distance;
+            if (!_groundPlane.Raycast(ray, out distance))
+            {
+                return Vector3.zero;
+            }
+            return ray.GetPoint(distance);
+        }
+
+        private bool ShouldUpdate()
+        {
+            if (Math.Abs(_oldZoom - _map.Zoom) < 0.001f) // camera move without zoom changing
+            {
+                return true;
+            }
+            else
+            {
+                if (_elapsedTime >= _cbtpOptions.updateInterval)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
 }
