773f6d5890d9021104c996a85d1ca19b677f7f16
Artem <artego@fuit.spbstu.ru>
2018-06-18 22:06:21 +0300

    UTG-157: add tabs to web view instead of pages

--- uTransnetCalcServer/src/main/java/com/github/utransnet/utranscalc/server/web/PresetView.java
+++ uTransnetCalcServer/src/main/java/com/github/utransnet/utranscalc/server/web/PresetView.java
@@ -5,6 +5,7 @@ import com.github.utransnet.utranscalc.server.data.services.MaterialService;
 import com.github.utransnet.utranscalc.server.data.services.PresetMaterialService;
 import com.github.utransnet.utranscalc.server.data.services.PresetService;
 import com.github.utransnet.utranscalc.server.web.components.PresetConfigGrid;
+import com.vaadin.navigator.View;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.Grid;
@@ -20,42 +21,29 @@ import java.util.Set;
 /**
  * Created by Artem on 04.06.2018.
  */
-
-@SpringUI(path = "preset")
-public class PresetUI extends UI {
+public class PresetView extends VerticalLayout implements View {
 
     private GridCrud<Material> materialsGrid = new GridCrud<>(Material.class);
 
 
-    private final PresetService presetService;
-    private final MaterialService materialService;
-    private final PresetMaterialService presetMaterialService;
 
-    public PresetUI(
+    public PresetView(
             PresetService presetService,
             MaterialService materialService,
             PresetMaterialService presetMaterialService
     ) {
-        this.presetService = presetService;
-        this.materialService = materialService;
-        this.presetMaterialService = presetMaterialService;
-    }
 
-    @Override
-    protected void init(VaadinRequest request) {
-        VerticalLayout layout = new VerticalLayout();
-
-        layout.setWidth("100%");
+        setWidth("100%");
 
         presetService.findAll().forEach(preset -> {
-            layout.addComponent(new PresetConfigGrid(
+            addComponent(new PresetConfigGrid(
                     preset,
                     presetMaterialService,
                     materialService,
                     presetService
             ));
         });
-        layout.addComponentsAndExpand(materialsGrid);
+        addComponentsAndExpand(materialsGrid);
 
 
 
@@ -76,6 +64,6 @@ public class PresetUI extends UI {
         materialsGrid.setUpdateOperation(materialService::saveAndFlush);
         materialsGrid.setDeleteOperation(materialService::delete);
 
-        setContent(layout);
+
     }
 }
--- uTransnetCalcServer/src/main/java/com/github/utransnet/utranscalc/server/web/WebUI.java
+++ uTransnetCalcServer/src/main/java/com/github/utransnet/utranscalc/server/web/WebUI.java
@@ -1,13 +1,7 @@
 package com.github.utransnet.utranscalc.server.web;
 
-import com.github.utransnet.utranscalc.server.data.BaseObject;
-import com.github.utransnet.utranscalc.server.data.services.BaseObjectMaterialService;
-import com.github.utransnet.utranscalc.server.data.services.BaseObjectService;
-import com.github.utransnet.utranscalc.server.data.services.MaterialService;
-import com.github.utransnet.utranscalc.server.data.services.PresetService;
+import com.github.utransnet.utranscalc.server.data.services.*;
 import com.github.utransnet.utranscalc.server.web.components.BaseObjectList;
-import com.github.utransnet.utranscalc.server.web.components.BaseObjectView;
-import com.vaadin.data.BinderValidationStatus;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.*;
@@ -23,65 +17,30 @@ public class WebUI extends UI {
     private final BaseObjectService baseObjectService;
     private final BaseObjectMaterialService baseObjectMaterialService;
     private final MaterialService materialService;
-    private BaseObjectList[] baseObjectList = new BaseObjectList[1];
+    private final PresetMaterialService presetMaterialService;
 
 
-    public WebUI(PresetService presetService, BaseObjectService baseObjectService, BaseObjectMaterialService baseObjectMaterialService, MaterialService materialService) {
+    public WebUI(
+            PresetService presetService,
+            BaseObjectService baseObjectService,
+            BaseObjectMaterialService baseObjectMaterialService,
+            MaterialService materialService,
+            PresetMaterialService presetMaterialService
+    ) {
         this.presetService = presetService;
         this.baseObjectService = baseObjectService;
         this.baseObjectMaterialService = baseObjectMaterialService;
         this.materialService = materialService;
+        this.presetMaterialService = presetMaterialService;
     }
 
     @Override
     protected void init(VaadinRequest request) {
-        VerticalLayout layout = new VerticalLayout();
-        layout.setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
-        baseObjectList[0] = new BaseObjectList(baseObjectService, baseObjectMaterialService, materialService);
-        layout.addComponent(baseObjectList[0]);
-
-
-        VerticalLayout popupContent = new VerticalLayout();
-
-
-        PopupView popup = new PopupView(null, popupContent);
-        popup.setHideOnMouseOut(false);
-        // Fill the pop-up content when it's popped up
-        popup.addPopupVisibilityListener(event -> {
-            if (event.isPopupVisible()) {
-                popupContent.removeAllComponents();
-                BaseObjectView baseObjectView = new BaseObjectView(new BaseObject(), baseObjectService, baseObjectMaterialService, materialService);
-                popupContent.addComponent(baseObjectView);
-                popupContent.addComponent(
-                        new Button(
-                                "Save",
-                                click -> {
-                                    BinderValidationStatus<BaseObject> status = baseObjectView.validate();
-
-                                    if (status.hasErrors()) {
-                                        Notification.show("Validation error count: "
-                                                + status.getValidationErrors().size());
-                                    } else {
-                                        try {
-                                            baseObjectService.save(baseObjectView.getBean());
-                                            layout.removeComponent(baseObjectList[0]);
-                                            baseObjectList[0] = new BaseObjectList(baseObjectService, baseObjectMaterialService, materialService);
-                                            layout.addComponentAsFirst(baseObjectList[0]);
-                                            popup.setPopupVisible(false);
-                                        } catch (Exception e) {
-                                            Notification.show(e.getMessage());
-                                        }
-                                    }
-                                }
-                        )
-                );
-            }});
-        Button button = new Button("Create new base object", click -> popup.setPopupVisible(true));
-
-
-        layout.addComponent(button);
-        layout.addComponent(popup);
-
-        setContent(layout);
+        TabSheet tabsheet = new TabSheet();
+        tabsheet.addTab(new ObjectsView(baseObjectService, baseObjectMaterialService, materialService))
+                .setCaption("Objects");
+        tabsheet.addTab(new PresetView(presetService, materialService, presetMaterialService))
+                .setCaption("Presets");
+        setContent(tabsheet);
     }
 }
